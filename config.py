917e4da83c6e87ccd7ca30c67cc73eec164a75f7
Sevmamra <sevmamra01@gmail.com>
2025-04-30 16:47:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,24 +1,24 @@
 import os
 
-API_ID = os.environ.get("API_ID", "12380656")
+API_ID = os.environ.get("API_ID", "26797881")
 
-API_HASH = os.environ.get("API_HASH", "d927c13beaaf5110f25c505b7c071273")
+API_HASH = os.environ.get("API_HASH", "9699262c708c2e45ba18bfce925ed5ed")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7129448604:AAHO_0qmWbacJKdr3ovcnJcTJOJCZI2QwWY")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8036885246:AAEj4iski-rVFYrkKCulka9TylaSFWe_yI4")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
-#OWNER = int(os.environ.get("OWNER", 7827463899))
+#OWNER = int(os.environ.get("OWNER", 6567162029))
 
-#LOG = -1002159628443,
+#LOG = -1002303981738,
 
 # UPDATE_GRP = , # bot sat group
 
 # auth_chats = []
 
 #try:
-    #ADMINS=[7827463899]
-    #for x in (os.environ.get("ADMINS", "7827463899").split()):
+    #ADMINS=[6286749445]
+    #for x in (os.environ.get("ADMINS", "6286749445").split()):
         #ADMINS.append(int(x))
 #except ValueError:
         #raise Exception("Your Admins list does not contain valid integers.")
